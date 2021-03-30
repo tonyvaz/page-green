b394619c095455a7acd703018b5b00f8e408fe3b
tonyvaz <antoniovazquezg1@gmail.com>
2021-03-30 09:57:58 -0600

    Implement functionallity for the left navigation in menu

--- pages/menu/script/menu.js
+++ pages/menu/script/menu.js
@@ -45,3 +45,52 @@ leftNavigation.addEventListener('click',function(e){
     }
 
 })
+
+
+//Create example of an Intersection Observer
+
+const changeItemActive = (entries, observer) => {
+    const leftMenuItems = document.querySelectorAll('.section__menu__left__item')
+
+    leftMenuItems.forEach((item)=> {
+        item.classList.remove('section__menu__left__item--active')
+    })
+
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            currentSection = entry.target.classList[entry.target.classList.length-1]
+            if(currentSection == 'seasonal'){
+                leftMenuItems[0].classList.add('section__menu__left__item--active')
+            }
+            else if(currentSection == 'bowls'){
+                leftMenuItems[1].classList.add('section__menu__left__item--active')
+            }
+            else if(currentSection == 'salads'){
+                leftMenuItems[2].classList.add('section__menu__left__item--active')
+            }
+            else if(currentSection == 'plates'){
+                leftMenuItems[3].classList.add('section__menu__left__item--active')
+            }
+            else if(currentSection == 'sides'){
+                leftMenuItems[4].classList.add('section__menu__left__item--active')
+            }
+
+            //console.log(entry.target.classList[entry.target.classList.length-1]) sides
+        }
+    })
+    
+}
+
+const options = {threshold:0.7}
+
+let seasonalObserver = new IntersectionObserver(changeItemActive , options)
+let sidesObserver = new IntersectionObserver(changeItemActive , options)
+let bowlsObserver = new IntersectionObserver(changeItemActive , options)
+let platesObserver = new IntersectionObserver(changeItemActive , options)
+let saladsObserver = new IntersectionObserver(changeItemActive , options)
+
+seasonalObserver.observe(document.querySelector('.section__menu.seasonal'))
+seasonalObserver.observe(document.querySelector('.section__menu.sides'))
+seasonalObserver.observe(document.querySelector('.section__menu.bowls'))
+seasonalObserver.observe(document.querySelector('.section__menu.plates'))
+seasonalObserver.observe(document.querySelector('.section__menu.salads'))
